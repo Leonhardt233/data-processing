a439c86b29209706ac10cdbca6ab4dacfc56e849
Leonhardt233 <156657189+Leonhardt233@users.noreply.github.com>
2024-02-08 11:37:52 +0800

    Update 1-2-股票分析.py

--- 数据分析/test1/1-2-股票分析.py
+++ 数据分析/test1/1-2-股票分析.py
@@ -1,9 +1,6 @@
-# 第一步
 # -*- coding: utf-8 -*-
-# 导入numpy库，并命名为np
 import numpy as np
 
-# 第二步
 # dict() 函数用于创建一个字典数据:dic_data1
 dic_data1 = dict(
     # fname表示读取的文件
@@ -20,7 +17,6 @@ dic_data1 = dict(
     unpack=True
 )
 
-# 第三步
 # 使用np.loadtxt方法读取字典dic_data1，并将指定列的数据分别赋值给col2,col3,col4,col5,col6
 col2, col3, col4, col5, col6 = np.loadtxt(**dic_data1)
 # 输出收盘价
@@ -28,28 +24,24 @@ print("收盘价=", col5)
 # 输出交易额
 print("交易额=", col6)
 
-# 第四步
 # 使用np.average方法计算平均价和加权平均价
 col5_average = np.average(col5)
 col6_wave = np.average(col5, weights=col6)
 print("平均价=", col5_average)
 print("加权平均价=", col6_wave)
 
-# 第五步
 # 使用np.max和np.min方法计算最高价和最低价
 col3_maxPrice = np.max(col3)
 col4_minPrice = np.min(col4)
 print("最高价=", col3_maxPrice)
 print("最低价=", col4_minPrice)
 
-# 第六步
 # 使用np.ptp方法计算最高差价和最低差价
 col3_highvalue = np.ptp(col3)
 col4_lowvalue = np.ptp(col4)
 print("最高差价=", col3_highvalue)
 print("最低差价=", col4_lowvalue)
 
-# 第七步
 # 使用np.median方法计算盘价的中位数
 col2_midPrice = np.median(col2)
 print("开盘价的中位数=", col2_midPrice)
@@ -57,7 +49,6 @@ print("开盘价的中位数=", col2_midPrice)
 col2_var = np.var(col2)
 print("开盘价的方差=", col2_var)
 
-# 第八步
 # 使用np.diff方法计算对数收益率
 col5_logearning = np.diff(np.log(col5))
 # 使用std()函数计算方差，mean()函数计算均值，sqrt()函数计算平方根
